1c762e41d1486a9085d9a506f8ebb9ebe5e7964f
Fraser Scott <fraser.scott@gmail.com>
2014-09-18 03:20:15 +0100

    first commit

---
+++ recipes/golang/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version="1.3.1"
+
+os=$(uname -s)
+kernel=$(uname -r)
+arch=$(uname -m)
+
+if [[ "$arch" = "x86_64" ]]; then
+  arch="amd64"
+else
+  arch="386"
+end
+
+puts "hi"
---
+++ src/api.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'webrick'
+
+class MyServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET (request, response)
+    host = request['Host'].split(':').first
+    if host =~ /install\.recipes$/
+      path = host.split('.')[0..-3].reverse.join('/')+'/install.sh'
+    else
+      path = 'help.txt'
+    end
+
+    response.status = 302
+    response['Location'] = "https://badger.com/#{path}"
+  end
+end
+
+server = WEBrick::HTTPServer.new(:Port => 1234)
+ 
+server.mount "/", MyServlet
+ 
+trap("INT") {
+    server.shutdown
+}
+ 
+server.start
